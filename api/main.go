476c68c7cdff1f3c307118daac475adaa656e81f
tinrab <tin.rabzelj@gmail.com>
2017-10-07 06:24:13 +0200

    Add api service

---
+++ api/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/rs/cors"
+
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
+)
+
+type Post struct {
+	Text      string    `json:"text"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+var posts *mgo.Collection
+
+func main() {
+	// Connect to mongo
+	session, err := mgo.Dial("mongo:27017")
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	// Get posts collection
+	posts = session.DB("app").C("posts")
+
+	// Set up routes
+	r := mux.NewRouter()
+	r.HandleFunc("/posts", createPost).
+		Methods("POST")
+	r.HandleFunc("/posts", readPosts).
+		Methods("GET")
+
+	http.ListenAndServe(":8080", cors.AllowAll().Handler(r))
+	log.Println("Listening on port 8080...")
+}
+
+func createPost(w http.ResponseWriter, r *http.Request) {
+	// Read body
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responseError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Read post
+	post := &Post{}
+	err = json.Unmarshal(data, post)
+	if err != nil {
+		responseError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	post.CreatedAt = time.Now().UTC()
+
+	// Insert new post
+	if err := posts.Insert(post); err != nil {
+		responseError(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	responseJSON(w, post)
+}
+
+func readPosts(w http.ResponseWriter, r *http.Request) {
+	result := []Post{}
+	if err := posts.Find(nil).Sort("-createdAt").All(&result); err != nil {
+		responseError(w, err.Error(), http.StatusInternalServerError)
+	} else {
+		responseJSON(w, result)
+	}
+}
+
+func responseError(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(map[string]string{"error": message})
+}
+
+func responseJSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
